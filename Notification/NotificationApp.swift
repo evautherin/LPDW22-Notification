e2c515b679972ef6522dd8b8d012c6f664c07bd3
Etienne Vautherin <etienne@vautherin.com>
2022-02-04 15:07:46 +0100

    Initial Commit

---
+++ Notification/NotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationApp.swift
+//  Notification
+//
+//  Created by Etienne Vautherin on 04/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct NotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
